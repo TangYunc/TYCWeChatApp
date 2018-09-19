961f6c6f8a705fa9c5d17cce1d9ade9e3054b5ea
yunchuan <tangyc@zgjzd.cn>
2018-09-19 16:29:30 +0800

    学习微信小程序，第一次提交

---
+++ pages/posts/post.js
@@ -0,0 +1,74 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var post_content1 = {
+      date: "Sep 19 2018",
+      title: "正是虾肥蟹壮时",
+      post_img: "/images/post/crab.png",
+      content: "对于自己不理解的事情要善于把自己跳出来，用旁观者的眼光去看别人怎么处理怎么经历。然后自己再进行模拟练习。虽然不能像你们所经历的那种生死恋什么的，但是，别人的故事能有所读，最后有所悟就行了",
+      view_num: "112",
+      collect_num: "96",
+      author_img: "/images/avatar/1.png",
+    }
+    this.setData(post_content1)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
