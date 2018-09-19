6d935917c604efe0cb08edb8ec9d7d62592dfce3
yunchuan <tangyc@zgjzd.cn>
2018-09-19 16:59:20 +0800

    数据绑定扩展

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -14,11 +14,16 @@ Page({
     var post_content1 = {
       date: "Sep 19 2018",
       title: "正是虾肥蟹壮时",
-      post_img: "/images/post/crab.png",
+      img: {
+        post_img: "/images/post/crab.png",
+        author_img: "/images/avatar/1.png"
+      },
+      img_condition: true,
+      a: 1,
+      b: 2,
       content: "对于自己不理解的事情要善于把自己跳出来，用旁观者的眼光去看别人怎么处理怎么经历。然后自己再进行模拟练习。虽然不能像你们所经历的那种生死恋什么的，但是，别人的故事能有所读，最后有所悟就行了",
       view_num: "112",
       collect_num: "96",
-      author_img: "/images/avatar/1.png",
     }
     this.setData(post_content1)
   },
