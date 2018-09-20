28154e3d1acaa4d791ab7932a75d8b9b9a3ee474
yunchuan <tangyc@zgjzd.cn>
2018-09-20 17:50:41 +0800

    缓存的四种设置八种方法，存取

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -4,10 +4,26 @@ Page({
   onLoad: function(option) {
     var postId = option.id;
     var postData = postsData.postList[postId];
+    // 如果在onLoad方法中，不是移不动去执行一个数据绑定
+    // 则不需要使用this.setData方法
+    // 只需要对this.data复制即可是实现数据绑定
     this.setData({
       postData: postData
     })
     // this.data.postData = postData;
+
+    wx.setStorageSync('key', {game:'风暴英雄',developer:'暴雪'})
+    wx.setStorageSync('key1', { game: 'LOL', developer: '拳头' })
+  },
+  onCollectionTap: function (event) {
+    var game = wx.getStorageSync('key')
+    console.log(game)
+  },
+
+  onShareTap:function(event) {
+    // wx.removeStorageSync('key')
+    //缓存的上限最大不能超过10MB
+    wx.clearStorageSync()
   }
 
 })
