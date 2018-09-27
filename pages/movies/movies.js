a2831956a0e2c93c0a396816b29174ed6ba327da
yunchuan <tangyc@zgjzd.cn>
2018-09-27 10:44:21 +0800

    获取正在热映、即将上映和top250的数据;电影页面数据绑定 上

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -3,16 +3,17 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var inTheatersUrl = app.globalData.doubanBase+ 'v2/movie/in_theaters'
-    var comingSoonUrl = app.globalData.doubanBase+ 'v2.movie/coming_soon'
-    var top250Url = app.globalData.doubanBase+ 'v2/movie/top250'
+  onLoad: function(options) {
+    var inTheatersUrl = app.globalData.doubanBase + 'v2/movie/in_theaters'
+    var comingSoonUrl = app.globalData.doubanBase + 'v2.movie/coming_soon'
+    var top250Url = app.globalData.doubanBase + 'v2/movie/top250'
     this.getMovieListData(inTheatersUrl)
-    this.getMovieListData(comingSoonUrl)
-    this.getMovieListData(top250Url)
+    // this.getMovieListData(comingSoonUrl)
+    // this.getMovieListData(top250Url)
   },
 
-  getMovieListData:function(url) {
+  getMovieListData: function(url) {
+    var that = this
     wx.request({
       url: url,
       method: 'GET',
@@ -20,9 +21,29 @@ Page({
         'content-type': 'application/json' // 默认值
       },
       success(res) {
-        console.log(res.data)
+        // console.log(res.data)
+        that.processDoubanData(res.data)
       }
     })
-  }
-
+  },
+  processDoubanData: function(moviesDouban) {
+    var movies = [];
+    for (var idx in moviesDouban.subjects) {
+      var subject = moviesDouban.subjects[idx]
+      var title = subject.title
+      if (this.length >= 6) {
+        title = title.substring(0, 6) + '...'
+      }
+      var temp = {
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
+      }
+      movies.push(temp)
+    }
+    this.setData({
+      movies: movies
+    })
+  },
 })
